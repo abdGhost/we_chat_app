b8287f3b6e82cecaa6e6ac9a17e3ddfe136a4cfe
abdul <ktaher642@gmail.com>
2023-10-03 11:31:51 +0530

    worked on send input field of chat Screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,60 +20,142 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   Widget appBar() {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: Row(
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: const Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(deviceSize.height * 0.3),
+              child: CachedNetworkImage(
+                width: deviceSize.height * .055,
+                height: deviceSize.height * .055,
+                fit: BoxFit.cover,
+                imageUrl: widget.chatUser.image,
+                errorWidget: (context, url, error) => const CircleAvatar(
+                  child: Icon(CupertinoIcons.person),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.chatUser.name,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text(
+                  'Last seen not avaiable',
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _chatInput() {
     return Padding(
-      padding: const EdgeInsets.only(left: 5, right: 5),
+      padding: EdgeInsets.symmetric(
+        vertical: deviceSize.height * 0.01,
+        horizontal: deviceSize.width * 0.025,
+      ),
       child: Row(
         children: [
-          InkWell(
-            onTap: () {
-              Navigator.of(context).pop();
-            },
+          Expanded(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  10,
+                ),
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.emoji_emotions,
+                      color: Colors.blue,
+                      size: 25,
+                    ),
+                  ),
+                  const Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                        hintText: 'Write something here...',
+                        hintStyle: TextStyle(
+                          color: Colors.blue,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.image,
+                      color: Colors.blue,
+                      size: 26,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.camera_alt_rounded,
+                      color: Colors.blue,
+                      size: 26,
+                    ),
+                  ),
+                  SizedBox(
+                    width: deviceSize.width * 0.02,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          MaterialButton(
+            minWidth: 0,
+            padding:
+                const EdgeInsets.only(top: 10, bottom: 10, right: 5, left: 10),
+            onPressed: () {},
+            shape: const CircleBorder(),
+            color: Colors.green,
             child: const Icon(
-              Icons.arrow_back,
-              color: Colors.black,
+              Icons.send,
+              size: 28,
+              color: Colors.white,
             ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(deviceSize.height * 0.3),
-            child: CachedNetworkImage(
-              width: deviceSize.height * .055,
-              height: deviceSize.height * .055,
-              fit: BoxFit.cover,
-              imageUrl: widget.chatUser.image,
-              errorWidget: (context, url, error) => const CircleAvatar(
-                child: Icon(CupertinoIcons.person),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.chatUser.name,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black54,
-                ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              const Text(
-                'Last seen not avaiable',
-                style: TextStyle(
-                  fontSize: 13,
-                  color: Colors.grey,
-                ),
-              ),
-            ],
           )
         ],
       ),
@@ -88,6 +170,11 @@ class _ChatScreenState extends State<ChatScreen> {
         appBar: AppBar(
           automaticallyImplyLeading: false,
           flexibleSpace: appBar(),
+        ),
+        body: Column(
+          children: [
+            _chatInput(),
+          ],
         ),
       ),
     );
